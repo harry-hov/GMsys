67d95b8537ea6b9a67f570f85bc687b90b18f050
Hariom Verma <hariom18599@gmail.com>
2019-05-31 21:10:40 +0530

    new home page

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,6 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('', views.home, name='blog-home'),
     path('home/', views.home, name='blog-home'),
+    path('about/', views.about, name='blog-about'),
+    path('contributors/', views.contributors, name='contributors'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
 	return render(request,'index.html')
+
+def about(request):
+	return render(request,'about.html')
+
+def contributors(request):
+	return render(request,'contributors.html')
