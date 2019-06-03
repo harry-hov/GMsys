975ddb8248f0a329d3cdbbd5709606a6e36f5db9
Hariom Verma <hariom18599@gmail.com>
2019-06-03 15:52:00 +0530

    models for grievance and minor gitcard fixes

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Grievance(models.Model):
+    name = models.CharField(max_length=20)
+    enroll = models.CharField(max_length=20)
+    depart = models.BooleanField()
+    msg = models.CharField(max_length=200)
