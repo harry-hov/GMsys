4791d912c8be78c6b50ade449162a3097078187f
Hariom Verma <hariom18599@gmail.com>
2019-06-05 18:48:32 +0530

    now have Grievance ID

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,4 @@
+import datetime
 from django.db import models
 
 # Create your models here.
@@ -10,7 +11,19 @@ STATUS=(
     ('Not Solved','Not Solved')
 )
 
+
+def grievance_number():
+    last_grievance = Grievance.objects.all().order_by('id').last()
+    if not last_grievance:
+        return 'GMS' + str(datetime.date.today().year) + str(datetime.date.today().month).zfill(2) + '0000'
+    grievance_id = last_grievance.grievance_id
+    grievance_int = int(grievance_id[9:13])
+    new_grievance_int = grievance_int + 1
+    new_grievance_id = 'GMS' + str(str(datetime.date.today().year)) + str(datetime.date.today().month).zfill(2) + str(new_grievance_int).zfill(4)
+    return new_grievance_id
+
 class Grievance(models.Model):
+    grievance_id = models.CharField(max_length = 20, editable=False, default=grievance_number)
     name = models.CharField(max_length=20)
     enroll = models.CharField(max_length=20)
     #depart = models.CharField(max_length=20)
@@ -20,4 +33,7 @@ class Grievance(models.Model):
     status = models.CharField(max_length=6, choices=STATUS, default='Not Solved')
     
     def __str__(self):
-        return self.enroll
+        #return self.enroll
+        return self.grievance_id
+
+    
